8ceca1b0b4ca9e09617f06e20fb65d3a2e24b378
soukouki <soukouki0@yahoo.co.jp>
2021-12-09 00:35:31 +0900

    選択ソートのミスを修正

--- src/algorithms-and-data-structure/04-selection-sort.py
+++ src/algorithms-and-data-structure/04-selection-sort.py
@@ -3,12 +3,13 @@ a = [1, 5, 2, 9, 4, 0]
 
 n = len(a)
 
-for i in range(0, n):
+for i in range(0, n-1):
   minj = i
-  for j in range(i+1, n):
+  for j in range(i, n):
     print(i, j, minj, a)
     if a[j] < a[minj]:
       minj = j
+  if i != minj:
     a[i], a[minj] = a[minj], a[i]
 
 print(a)
