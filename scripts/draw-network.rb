c387bc3f0a6484c645d0c722a9066c060d4f2437
soukouki <soukouki0@yahoo.co.jp>
2021-12-23 09:29:00 +0900

    ネットワークを表示させたりしてみた。どこかで使いたい

---
+++ scripts/draw-network.rb
@@ -0,0 +1,25 @@
+
+require 'yaml'
+
+index = YAML.load_file('tmp/merged_article_list.yaml')
+
+files_map = index[:files].map{|arr|[arr[:path], arr[:title]]}.to_h
+
+network = YAML.load_file('tmp/merged_network.yaml')
+
+body = network
+  .flat_map do |pre_name, pre_arr|
+    pre_arr.map{|nex_item|"\"#{files_map[pre_name]}\" -> \"#{files_map[nex_item[:path]]}\""}
+  end
+  .join("\n")
+
+puts <<~DOT
+  digraph {
+    overlap = scale;
+    #{body}
+  }
+DOT
+
+# ruby scripts/draw-network.rb > a.dot
+
+# dot a.dot -Tpng -Kneato -o network.png
