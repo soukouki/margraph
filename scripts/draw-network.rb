08af2fbc508cfd24e3199b2f2aad3ee6517ff713
soukouki <soukouki0@yahoo.co.jp>
2022-01-21 11:43:47 +0900

    draw-networkが壊れていたので修正ついでに改良

--- scripts/draw-network.rb
+++ scripts/draw-network.rb
@@ -3,13 +3,13 @@ require 'json'
 
 index = JSON.load_file('tmp/merged-articles.json')
 
-files_map = index['files'].map{|arr|[arr['path'], arr['title']]}.to_h
+files_map = index['files'].map{|path, arr|[path, arr['title']]}.to_h
 
 network = JSON.load_file('tmp/merged-link-network.json')
 
 body = network
   .flat_map do |pre_name, pre_arr|
-    pre_arr.map{|nex_item|"\"#{files_map[pre_name]}\" -> \"#{files_map[nex_item['path']]}\""}
+    pre_arr.map{|nex_item|"\"#{files_map[pre_name]}\" -> \"#{files_map[nex_item['path']]}\""}.uniq
   end
   .join("\n")
 
