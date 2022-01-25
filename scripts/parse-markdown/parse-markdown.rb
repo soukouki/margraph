b1e1b4dc09f72c14b25c28fcefc77197ac7a7b79
soukouki <soukouki0@yahoo.co.jp>
2022-01-26 00:54:35 +0900

    一行のコードが表示されていなかったのを修正

--- scripts/parse-markdown/parse-markdown.rb
+++ scripts/parse-markdown/parse-markdown.rb
@@ -34,7 +34,12 @@ class InternalJson < Kramdown::Converter::Html
         text: el.children[0].value
       }]
     when :p
-      if el.children.length == 1 && el.children[0].type == :codespan
+      is_codeblock = (
+        el.children.length == 1 &&
+        el.children[0].type == :codespan &&
+        el.children[0].value.include?("\n")
+      )
+      if is_codeblock
         code = el.children[0].value
         lang = code.start_with?(/[a-zA-Z#+]+|.+\.[a-zA-Z]+/) ? code.lines[0].chomp : nil
         return [{
