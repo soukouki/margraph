e381146bfccb6ddcc10a474a3bc8135b54ff3762
soukouki <soukouki0@yahoo.co.jp>
2022-04-18 14:35:53 +0900

    Revert "フォントを指定子てみた"
    This reverts commit e55eb40770db8bb402ecfa64fa4e6811c76521b1.

--- scripts/draw-network.rb
+++ scripts/draw-network.rb
@@ -19,9 +19,6 @@ body = network
 
 puts <<~DOT
   digraph {
-    graph [fontname = "IPAGothic"];
-    node [fontname = "IPAGothic"];
-    edge [fontname = "IPAGothic"];
     layout = fdp;
     #{body}
   }
